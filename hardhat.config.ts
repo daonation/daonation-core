b7960db54767275a5cbca396226f052d91127a37
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-09-10 17:53:02 +0000

    Initial commit.

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_URL,
+      chainId: 1,
+      gasPrice: 5000000000,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      chainId: 5,
+      gasPrice: 5000000000,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }
+    },
+  }
+};
+
+export default config;
